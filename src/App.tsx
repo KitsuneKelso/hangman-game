53ce2ff99d28b8529724e04bd94f559bdd4199f3
Fredrik Gustafsson <fredrik.gustafsson@regent.se>
2023-01-25 13:53:06 +0100

    experiment with react-query v4

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
 import { Hangman } from "./components";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hangman</h1>
-      <Hangman />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Hangman</h1>
+        <Hangman />
+      </div>
+    </QueryClientProvider>
   );
 }
 
